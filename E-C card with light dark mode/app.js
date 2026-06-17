b91a4a4bcdebe6ffc89c1a23502dea918b64248b
Saqibcoder99 <javaidsaqib853@gmail.com>
2026-06-17 09:58:51 +0500

    updated

--- E-C card with light dark mode/app.js
+++ E-C card with light dark mode/app.js
@@ -1,21 +1,48 @@
+const togggleButton = document.getElementById("toggle-button")
+const theme = localStorage.getItem("theme")
+const icon = document.getElementById("myIcon");
+const size= document.getElementById("size")
+const sizeColor=size.getElementsByTagName("span")
 function myFunction(smallImg) {
     var fullImg = document.getElementById("full")
     fullImg.src = smallImg.src;
 }
 
 function changeColor() {
-    const icon = document.getElementById("myIcon");
     // Check current icon class and switch
     if (icon.classList.contains("fa-toggle-off")) {
+        localStorage.setItem("theme", "dark")
+        window.location.reload()
+    }
+    else {
+        localStorage.setItem("theme", "light")
+        window.location.reload()
+    }
+}
+
+
+const loadApp = () => {
+
+    if (theme == "dark") {
         icon.classList.remove("fa-toggle-off");
         icon.classList.add("fa-toggle-on");
         document.body.style.backgroundColor = " #352f2f"
-           icon.style.color="white"
-        
-    } else {
+        icon.style.color = "white"
+        document.body.style.color="white"
+        for(i=0;i<sizeColor.length;i++){
+            sizeColor[i].style.color="white"
+        }
+    }
+
+    if (theme == "light") {
         icon.classList.remove("fa-toggle-on");
         icon.classList.add("fa-toggle-off");
         document.body.style.backgroundColor = "white"
-        icon.style.color="black"
+        icon.style.color = "black"
     }
 }
+
+loadApp()
+
+
+togggleButton.addEventListener("click", changeColor)
